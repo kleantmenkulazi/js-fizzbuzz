4432c87f422f9f363ceef12735dd2432338bb605
kleantmenkulazi <kleantmenkulazi@gmail.com>
2024-07-18 06:28:02 +0200

    Esercizio completato

--- js/script.js
+++ js/script.js
@@ -1,21 +1,27 @@
 console.log('Js collegato');
 
+const myList = document.querySelector('ol');
+
 for (
     let i = 1;
     i != 101;
     i++
 ) {
+    const newLi = document.createElement('li');
     if (i % 3 == 0 && i % 5 == 0) {
-        console.log('FizzBuzz');
+        newLi.append('FizzBuzz');
+        newLi.classList.add('fizzbuzz');
 }
 else if (i % 3 == 0) {
-    console.log('Fizz');
+    newLi.append('Fizz');
+    newLi.classList.add('fizz');
 }
 else if (i % 5 == 0) {
-    console.log('Buzz');
+    newLi.append('Buzz');
+    newLi.classList.add('buzz');
 }
 else {
-    console.log(i);
+    newLi.append(i);
 }
-
+ myList.append(newLi);
 }
